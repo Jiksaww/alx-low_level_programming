eda59fc4ad779a002b7e4f34b07bd5b0e91687c5
Jiksaww <rajae5741@gmail.com>
2023-03-16 14:15:33 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always0(Success)
 */
 int main(void)
 {
-printf("\"Programming is like building a multilingual puzzle,\n");
+printf("with proper grammer,but the outcome is a piece of art,\n");
 return (0);
 }
 
