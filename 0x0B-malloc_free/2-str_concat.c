dd8cb26178da7b55c8535772ca6347cd0abd0d4a
Jiksaww <elouardanirajae@gmail.com>
2023-04-12 05:47:31 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,83 +1,93 @@
-#include "main.h"
-		
 #include <stdlib.h>
-		
-/**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+#include <string.h>
+
+/*
+ *       NOTE: I had to split my function into two
+ *       to comply with the betty doc style
+ *       of not more than 40 lines per fucntion
  */
-		
-char *str_concat(char *s1, char *s2)
-		
+
+/**
+ * conditional - code snippet for function concat
+ *
+ * @s1: address of s1
+ * @s2: address of s2
+ * @s1len: length of s1
+ * @s2len: length of s2
+ * @check: length of s3
+ */
+
+void conditional(char *s1, char *s2, int *s1len, int *s2len, int *check)
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
-
-		
 	if (s1 == NULL)
-		
-		s1 = "";
-		
+	{
+		*s1len = 0;
+		*check = 1;
+	}
+
+	else
+	{
+		*s1len = strlen(s1);
+	}
+
 	if (s2 == NULL)
-		
-		s2 = "";
-		
+	{
+		*s2len = 1;
+		*check = 2;
+	}
+	else
+	{
+		*s2len = strlen(s2);
+	}
+}
 
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
+/**
+ * *str_concat - concatenate provided strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to first string or null on failure
+ */
 
-		
-	if (conct == NULL)
-		
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, s1len = 0, s2len = 0, check = 0;
+	char *newstr;
+
+	conditional(s1, s2, &s1len, &s2len, &check);
+
+	/* allocate memory to newstr */
+	newstr = malloc((s1len + s2len + 1) * sizeof(char));
+
+	if (newstr == NULL)
 		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
-		
 
-		
-	while (s2[ci] != '\0')
-		
+	if (s1 == NULL && s2 == NULL)
+		return (strdup(""));
+
+	/* iterate over and assign values of first string */
+	for (i = 0; i < (s1len + s2len); i++)
 	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
+		if (i < s1len)
+		{
+			if (check == 2 || check == 0)
+				newstr[i] = s1[i];
+
+			if (check == 1)
+				newstr[i] = (char) 0;
+
+			continue;
+		}
+		if (check == 1 || check == 0)
+			newstr[i] = s2[j];
+
+		if (check == 2)
+			newstr[i] = (char) 0;
+		j++;
 	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+
+	newstr[i] = '\0';
+
+	return (newstr);
 }
