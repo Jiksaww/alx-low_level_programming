9e4763a30939edd739f3d916abde466ebcb56a1e
Jiksaww <rajae5741@gmail.com>
2023-04-27 11:45:10 -0700

    tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,37 @@
 #include "lists.h"
 
 /**
- * print_list - a function that prints all the elements of a list_t list.
- * @h: linked list
- * Return: number of nodes in linked list
+ * _strlen - returns the length of the string.
+ * @s: the string whose length to check././
+ *
+ * Return: integer length of string
  */
+int _strlen(char *s)
+{
+	int i = 0;
 
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_list - prints a linked lists/linked lists.lists
+ * @h: /pointer to the first node..
+ *
+ * Return:the  size of the list
+ */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		count++;
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
+		i++;
 	}
-
-	return (count);
+	return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h> /* size_t, printf */
-#include <stdlib.h> /* malloc */
-#include <string.h> /* strdup */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -21,11 +21,13 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* function prototypes for all files */
+int _putchar(char c);
+int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void _constructor(void) __attribute__ ((constructor));
 
 #endif
